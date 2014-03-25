1763f2f86a6def8131a1f7c38a0030857437cedb
Tom Robertson <tomasrobertson@ymail.com>
2014-03-25 15:57:57 +0000

    using checkpoints

--- controllers/advanced_genetic_algorithm/advanced_genetic_algorithm.c
+++ controllers/advanced_genetic_algorithm/advanced_genetic_algorithm.c
@@ -12,6 +12,8 @@
 #include <assert.h>
 #include <string.h>
 #include <stdio.h>
+#include <math.h>
+#include <webots/gps.h>
 
 #define NUM_SENSORS 11
 #define NUM_WHEELS 2
@@ -30,16 +32,43 @@ double matrix[NUM_SENSORS][NUM_WHEELS];
 
 #define NB_DIST_SENS 8
 
+#define NUM_CHECKS 17
+
 unsigned short gs_value[NB_GROUND_SENS]={0,0,0};
 
 WbDeviceTag sensors[NUM_SENSORS];  // proximity sensors
 WbDeviceTag receiver;              // for receiving genes from Supervisor
 WbDeviceTag robot_emitter;
 WbDeviceTag robot;
+WbDeviceTag gps;
 
 double f[1];
 int mili = 0;
 double cur_x, prev_x, cur_y, prev_y;
+
+int cur_check = 0;
+
+double checks_x[NUM_CHECKS] = {
+//turn 1
+0.246938,  0.245117, 0.213594,  0.054236, -0.150587, -0.243441,
+//ob 1
+-0.192012, -0.170405, -0.139799, -0.15743,
+//turn 2
+-0.212677, -0.0907201, 0.0653666,
+//ob 2
+0.0893589, 0.153887, 0.212742
+};
+double checks_z[NUM_CHECKS] = {
+//turn 1
+-0.042318, 0.049354, 0.207657,  0.298794, 0.273649,  0.176932,
+//ob 1
+0.0702441, 0.0473333, -0.00430759, -0.0220507,
+//turn 2
+-0.215883, -0.273285, -0.280998,
+//ob 2
+-0.189704, -0.184477, -0.211189
+};
+
 
 // check if a new set of genes was sent by the Supervisor
 // in this case start using these new genes immediately
@@ -47,6 +76,7 @@ void check_for_new_genes() {
   if (wb_receiver_get_queue_length(receiver) > 0) {
   
     f[0] = 0;
+    cur_check = 0;
     // check that the number of genes received match what is expected
     assert(wb_receiver_get_data_size(receiver) == GENOTYPE_SIZE * sizeof(double));
     
@@ -76,31 +106,56 @@ void sense_compute_and_actuate() {
   for (i = 0; i < NUM_SENSORS; i++)
     sensor_values[i] = wb_distance_sensor_get_value(sensors[i]);
     
+  
   // Report results to supervisor
   for(i = NB_DIST_SENS; i < NUM_SENSORS; i ++) gs_value[i - NB_DIST_SENS] = sensor_values[i];
   
-  
-  if(gs_value[1] < 400) f[0] ++;
-  else f[0] -= 1;
-  if(gs_value[0] < 400) f[0] ++;
-  else f[0] -= 1;
-  if(gs_value[2] < 400) f[0] ++;
-  else f[0] -= 1;
-  
-  if(gs_value[1] > 400 && gs_value[1] < 900) f[0] -= 100;
-  
-  
+  int touching = 0;
   for(i=0; i < NB_DIST_SENS; i ++) {
-    if(sensor_values[i] > 2000) f[0] -= 1;
+    if(sensor_values[i] > 2000) touching = 1;
   }
-  mili ++;
-  if( mili > 1000){
-    mili = 0;
-    
-    prev_y = 
-    
-    
-    if(abs(prev_x - cur_x) < 0.01 && abs(prev_y - cur_y) < 0.01) f[0] -= 1000;
+  
+  /*
+  for(i=0; i < 3; i ++) {
+    if(gs_value[i] > 400 && gs_value[i] < 900) f[0] -= 2;
+    printf("gs: %d\n", gs_value[i]);
+  }
+  printf("\n");
+  */
+  
+  if(touching) {
+    for(i=0; i < 3; i ++) if(gs_value[i] < 500) f[0] -= 100;
+  }
+  
+  if(!touching) {
+    //printf("NOT touching\n");
+    if(gs_value[1] < 400) f[0] ++;
+    else f[0] -= 1;
+    if(gs_value[0] < 400) f[0] += 10;
+    else  f[0] -= 2;
+    if(gs_value[2] < 400) f[0] ++;
+    else f[0] -= 1;
+  }
+  
+  const double *cur_pos = wb_gps_get_values(gps);
+  
+  for(i=0; i < 3; i ++) {
+  
+    double dist = sqrt(pow((cur_pos[0] - checks_x[cur_check + i]), 2) + pow((cur_pos[2] - checks_z[cur_check + i]), 2));
+
+  /*
+  printf("d: %f\n", dist);
+  printf("cc: %d\n", cur_check);
+  printf("x: %f z: %f\n", cur_pos[0], cur_pos[2]);
+  printf("x: %f z: %f\n", checks_x[cur_check], checks_z[cur_check]);
+  */
+    if(dist < 0.02) {
+      f[0] += 500;
+      printf("cc: %d\n", cur_check);
+      if(cur_check < NUM_CHECKS) cur_check ++;
+      else cur_check = 0;
+      break;
+    }
   }
   
   
@@ -151,12 +206,15 @@ int main(int argc, const char *argv[]) {
   receiver = wb_robot_get_device("receiver");
   wb_receiver_enable(receiver, time_step);
   
-  robot = wb_robot_get_device("EPUCK");
+  robot = wb_robot_get_device("e-puck");
   
   
   
   //find and enable robot_emitter
   robot_emitter = wb_robot_get_device("emitter");
+  
+  gps = wb_robot_get_device("gps");
+  wb_gps_enable(gps, 1);
   
   // initialize matrix to zero, hence the robot 
   // wheels will initially be stopped
--- controllers/advanced_genetic_algorithm/build/advanced_genetic_algorithm.d
+++ controllers/advanced_genetic_algorithm/build/advanced_genetic_algorithm.d
@@ -5,4 +5,5 @@ build/release/advanced_genetic_algorithm.o: advanced_genetic_algorithm.c \
  C:/Program\ Files\ (x86)/Webots/include/controller/c/webots/differential_wheels.h \
  C:/Program\ Files\ (x86)/Webots/include/controller/c/webots/receiver.h \
  C:/Program\ Files\ (x86)/Webots/include/controller/c/webots/emitter.h \
- C:/Program\ Files\ (x86)/Webots/include/controller/c/webots/distance_sensor.h
+ C:/Program\ Files\ (x86)/Webots/include/controller/c/webots/distance_sensor.h \
+ C:/Program\ Files\ (x86)/Webots/include/controller/c/webots/gps.h
